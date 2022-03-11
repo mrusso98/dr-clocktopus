517e35d02cc282e5a423b3f2d8e2c5db40b300f3
Michael Russo <mwr264@nyu.edu>
2022-03-10 23:21:33 -0500

    Added basic RTC functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,32 @@
 #include <Arduino.h>
+#include <RTClib.h>
+#include <SPI.h>
 
 #define LED 13
+RTC_DS3231 rtc;
+
 
 void setup() {
-  pinMode(LED, OUTPUT);
+  // pinMode(LED, OUTPUT);
+
+  Serial.begin(9600);
+
+  // initializing the rtc
+  if(!rtc.begin()) {
+      Serial.println("Couldn't find RTC!");
+      Serial.flush();
+      while (1) delay(10);
+  }
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+
+  Serial.println("This is the beginning");
 }
 
 void loop() {
-  digitalWrite(LED, HIGH);
-  delay(300);
-  digitalWrite(LED, LOW);
-  delay(300);
+  // print current time
+  char date[10] = "hh:mm:ss";
+  rtc.now().toString(date);
+  Serial.println(date); 
+  //Serial.println("Test");
+  delay(2000);
 }
